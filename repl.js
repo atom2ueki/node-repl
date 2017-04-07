b66a537e92b83d98024de0b330afd8392f26583d
Tony Li <atom2ueki@gmail.com>
2017-04-07 16:13:05 +0800

    fixed root path issue

--- repl.js
+++ repl.js
@@ -9,5 +9,8 @@ var replServer = repl.start({
 var config = require("/Users/atom2ueki/Documents/atom2ueki/node-repl/app/config.js");
 replServer.context.config = config;
 
+var tony = require("/Users/atom2ueki/Documents/atom2ueki/node-repl/app/tony.js");
+replServer.context.tony = tony;
+
 var abc = require("/Users/atom2ueki/Documents/atom2ueki/node-repl/abc.js");
 replServer.context.abc = abc;
--- watch.js
+++
@@ -1,9 +0,0 @@
-/*!
- * repl.js
- * Licensed under the MIT license
- *
- * Copyright (c) 2016 Tony Li
- */
-"use strict"
-
-// keep tracking file changes
